d89f1bf31c8deb1c844b637e034baecd676d1518
simongarisch <gatman946@gmail.com>
2018-07-16 19:40:40 +1000

    Adding more tests for the StylePickerWidget class

--- qstyles/tests/test_style_picker_widget.py
+++ qstyles/tests/test_style_picker_widget.py
@@ -6,17 +6,24 @@ from qstyles import StylePicker, StylePickerWidget
 
 
 class TestStylePickerWidget(object):
-    
-    def setup_method(self, method):
+        
+    def test_displayed_styles(self, qtbot):
+        # test that all of the available styles are displayed
         self.picker = StylePicker()
         self.picker_widget = StylePickerWidget()
-        
-    def teardown_method(self, method):
-        # clear the attributes
-        del self.picker
-        
-    def test_init(self, qtbot):
         qtbot.addWidget(self.picker_widget)
-        style_count = self.picker.
-        assert isinstance(self.picker, QtWidgets.QComboBox)
+        available_styles = sorted(self.picker.available_styles)
+        displayed_styles = sorted([self.picker_widget.itemText(i) 
+                                   for i in range(self.picker_widget.count())])
+        assert available_styles == displayed_styles
+        
+    def test_change_app_style(self, qtbot):
+        # check that the style gets updated
+        self.picker = StylePicker()
+        self.picker_widget = StylePickerWidget()
+        qtbot.addWidget(self.picker_widget)
+        text = self.picker_widget.itemText(0)
+        self.picker_widget.setCurrentIndex(0)
+        assert self.picker_widget.currentText == text
+        # also check the app style sheet is changed
         
