86d7d4d625474dd014ff3596753339f772cbb920
simongarisch <gatman946@gmail.com>
2018-07-20 09:03:04 +1000

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name="qtstyles",
+      version="0.0.1",
+      install_requires=[
+          "QtPy>=1.4.1"
+        ],
+      description="A collection of Qt style sheets and helpful classes for applying them.",
+      long_description=open("README.md").read(),
+      author="Simon Garisch",
+      author_email="gatman946@gmail.com",
+      packages=find_packages()
+     )
