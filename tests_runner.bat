65dc460b88fa827fd212761ec3e179a5fa16e2f4
simongarisch <gatman946@gmail.com>
2019-04-24 17:11:55 +1000

    Modifying calls to pytest

--- tests_runner.bat
+++ tests_runner.bat
@@ -1,7 +1,7 @@
 call activate py27
-py.test --cov-report html --cov --doctest-modules --cache-clear
+pytest --cov-report html --cov --doctest-modules --cache-clear
 
 call activate py36
-py.test --cov-report html --cov --doctest-modules --cache-clear
+pytest --cov-report html --cov --doctest-modules --cache-clear
 
 pause
